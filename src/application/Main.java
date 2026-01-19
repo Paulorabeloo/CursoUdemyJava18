e30521e9543ca09d94ee44d30a9cdde9b68baa5f
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2026-01-19 07:57:38 -0300

    ForEach

---
+++ src/application/Main.java
@@ -0,0 +1,19 @@
+package application;
+
+class Main {
+    public static void main(String[] args) {
+
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+        for(int i=0; i< vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+
+        System.out.println("--------------");
+
+        //for each //// "Para cada objeto 'nomes' contido em vect, faça: "
+        for(String nomes : vect) {
+            System.out.println(nomes);
+        }
+    }
+}
